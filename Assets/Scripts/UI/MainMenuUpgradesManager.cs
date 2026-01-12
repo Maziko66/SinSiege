b09e6ba7fd1ad7c5957fc1b1b557c011275b61d9
Maziko66 <maziko66@gmail.com>
2026-01-12 14:04:43 +0300

    card changes

---
+++ Assets/Scripts/UI/MainMenuUpgradesManager.cs
@@ -0,0 +1,14 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuUpgradesManager : MonoBehaviour
+{
+    [Header("Character Info UI")]
+    [SerializeField] private TextMeshProUGUI textCharacterName;
+    [SerializeField] private TextMeshProUGUI textCharacterFullName;
+    [SerializeField] private TextMeshProUGUI textCharacterDescription;
+    [SerializeField] private Image charPortrait;
+    
+    
+}
